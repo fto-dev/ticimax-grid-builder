5fefd14fe09d84affc82ce31e3c41cda237c0463
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2022-08-28 19:32:20 +0300

    gh pages

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { grid , sortable} from "uikit";
-
-//import './assets/main.css'
-
-createApp(App).use(grid).use(sortable).mount('#app')
